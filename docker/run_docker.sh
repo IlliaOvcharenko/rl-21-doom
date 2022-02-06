ab684ba24bf69bd2ae015b3751215d82e4ed9290
Illia Ovcharenko <i.ovcharenko@intetics.com>
2022-02-06 15:37:27 +0200

    install pytorch

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,3 +1,8 @@
 docker build --build-arg UID=$UID -t rl-21-doom-image .
-docker run --name rl-21-doom-container --gpus all --shm-size=11g -dt -v $(pwd)/../:/home/dev/codebase/ -p 5900:5900 rl-21-doom-image
+docker run --name rl-21-doom-container \
+           --gpus all --shm-size=11g \
+           -dt -v $(pwd)/../:/home/dev/codebase/ \
+           -p 5900:5900 \
+           -w /home/dev/codebase \
+           rl-21-doom-image
 
