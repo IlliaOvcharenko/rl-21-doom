8638ae8d1b791da2a57c62379dc7f904d7ca07bc
Illia Ovcharenko <i.ovcharenko@intetics.com>
2022-02-06 14:53:34 +0200

    fix virtual display

--- run_virtual_display.sh
+++ run_virtual_display.sh
@@ -2,7 +2,6 @@
 # Run virtual XVBF  display and start VNC to braodcast it outside docker container
 # Use any VNC viewer and port 5900 to connect
 
-export DISPLAY=:1
 sudo  Xvfb "$DISPLAY" -screen 0 1024x768x24 &
 x11vnc -display "$DISPLAY" -bg -nopw -repeat -listen "$(hostname)" -xkb
 
